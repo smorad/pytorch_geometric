76e840149691c0d3385d2cda4da435a3a8bac76a
S. Morad <sm2558@cam.ac.uk>
2021-10-20 18:31:09 +0000

    Grids work with genagg

--- graphgym/run_batch.sh
+++ graphgym/run_batch.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-CONFIG=example_node
-GRID=example
-REPEAT=3
-MAX_JOBS=8
+CONFIG=graphconv_genagg
+GRID=genagg
+REPEAT=1
+MAX_JOBS=4
 SLEEP=1
 MAIN=main
 
 # generate configs (after controlling computational budget)
 # please remove --config_budget, if don't control computational budget
-python configs_gen.py --config configs/pyg/${CONFIG}.yaml \
+python3 configs_gen.py --config configs/pyg/${CONFIG}.yaml \
   --grid grids/pyg/${GRID}.txt \
   --out_dir configs
 #python configs_gen.py --config configs/ChemKG/${CONFIG}.yaml --config_budget configs/ChemKG/${CONFIG}.yaml --grid grids/ChemKG/${GRID}.txt --out_dir configs
@@ -22,4 +22,4 @@ bash parallel.sh configs/${CONFIG}_grid_${GRID} $REPEAT $MAX_JOBS $SLEEP $MAIN
 bash parallel.sh configs/${CONFIG}_grid_${GRID} $REPEAT $MAX_JOBS $SLEEP $MAIN
 
 # aggregate results for the batch
-python agg_batch.py --dir results/${CONFIG}_grid_${GRID}
+python3 agg_batch.py --dir results/${CONFIG}_grid_${GRID}
